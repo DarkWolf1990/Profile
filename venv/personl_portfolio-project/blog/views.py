1c2a2730ba40d3953de035b8c7c1bd5bd117f160
YOUR_NAME <YOUR_EMAIL>
2022-10-04 16:27:36 +0300

    update views.py

--- venv/personl_portfolio-project/blog/views.py
+++ venv/personl_portfolio-project/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def all_blogs(request):
@@ -8,4 +8,6 @@ def all_blogs(request):
 
 
 
-
+def detail(request, blog_id):
+  blog = get_object_or_404(Blog, pk=blog_id)
+  return render(request, 'blog/detail.html', {'blog':blog})
