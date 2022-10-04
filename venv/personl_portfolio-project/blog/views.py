7bb84373c6d0b2eaa9da17e86c4c9fc80493e59a
YOUR_NAME <YOUR_EMAIL>
2022-10-04 14:15:21 +0300

    update views.py

--- venv/personl_portfolio-project/blog/views.py
+++ venv/personl_portfolio-project/blog/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from .models import Blog
 
 def all_blogs(request):
+  # эта функция осуществляет сортировку от старой даты к новой
   blogs = Blog.objects.all().order_by('-date')[:5]
   return render(request, 'blog/all_blogs.html', {'blogs':blogs})
+
+
+
+
